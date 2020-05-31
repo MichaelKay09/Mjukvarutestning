4405f8a2138e3ff966abbce54441fceb6f8d8c36
Michael Kamara <56924747+MichaelKay09@users.noreply.github.com>
2020-05-31 14:32:54 +0200

    Update Budget.py

--- Budget.py
+++ Budget.py
@@ -4,23 +4,51 @@ from datetime import date
 
 
 class Budget():
+    budget_id = 0
+    budget_name = ""
+    budget_amount = 0
+    budgetlist = []
+    date = 0
 
-    def setBudget(self):
-        self.budget = int(input("\nPlease enter your budget today: "))
-        self.budget = 0
-        self.budgetlist = []
-        self.date = date
-        self.datelist = []
-
+    def setBudget(self, name, amount):
         try:
-            if self.budget <= 0:
-                print("Please be serious and enter your budget for today!\n")
-                self.setBudget()
+            if(amount <= 0):
+                print("Please enter amount in right format")
+
             else:
-                # "SPARA BUDGETEN I PROGRAMMET SÅ DEN KAN FÖLJA MED?"
-                self.budget_list.append(self.budget)
-                # Lägg till i TOTALT(expenses)
-                print(self.budget, "kr  Budget has been added!", "  ",  date.today(), "\n\n")
+                self.budget_name = name
+                self.budget_amount = int(amount)
+                Budget.budgetlist.append(
+                    [self.budget_name, self.budget_amount, self.date, self.budget_id])
+                Budget.budget_id += 1
+                print("Budget for {} has been created and {} has been added to it".format(
+                    self.budget_name, self.budget_amount))
         except:
-            print("Please enter your budget!\n")
-            self.setBudget()
+            print("Error!!!")
+
+    def seeHistory(self):
+        num = 0
+        if(len(Budget.budgetlist) <= 0):
+            print("No records has been registered!!!")
+        else:
+            for i in Budget.budgetlist:
+                print(str("{}. {}".format(num, i[:2])))
+                num += 1
+
+    def find_budget_name(self, num):
+        my_num = int(num)
+        return str(Budget.budgetlist[my_num][0])
+
+    def update_budget(self, index, amount):
+        if(amount <= 0):
+            print("ERROR COULD NOT ADD TRANSACTION!!!")
+
+        else:
+            indx = int(index)
+            mybudget = Budget.budgetlist[indx][1]
+            if(mybudget < amount):
+                print("Not enough Money in your account!!!")
+            else:
+                nybudget = mybudget - amount
+                Budget.budgetlist[indx][1] = nybudget
+                print("Transaction Added")
