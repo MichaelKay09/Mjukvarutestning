231bd791021b655ad248beee43e764812be58c0c
Michael Kamara <56924747+MichaelKay09@users.noreply.github.com>
2020-05-31 14:32:46 +0200

    Update Ultimatetracker.py

--- Ultimatetracker.py
+++ Ultimatetracker.py
@@ -1,46 +1,64 @@
 ##### MAIN MENU #####
 from Budget import Budget
 from Transaction import Transaction
+from datetime import date
 
-class Ultimatetracker(Budget):
 
-  def init(self):
-        self.mainMenu()
+def mainMenu():
+    my_budget = Budget()
+    my_record = Transaction()
 
-  def mainMenu(self):
+    while(True):
+        print("\n--------------------------MENU-------------------------------")
+        print("1 = SET BUDGET")
+        print("2 = SET TRANSACTION")
+        print("3 = SEE TRANSACTION HISTORY")
+        print("4 = SEE BUDGET HISTORY")
+        print("5 = EXIT")
+        print("-------------------------------------------------------------\n")
 
-        print("** WELCOME TO ULTIMATE TRACKER **\n")
-        print("WHAT DO YOU WANT TO DAY?:")
+        selection = 0
         try:
-
-            print("1 = SET BUDGET")
-            print("2 = SET TRANSACTION")
-            print("3 = SEE HISTORY")
-            print("4 = EXIT")
-
-            selection=int(input("Enter 1-4: "))
-
-
-            if selection==1: 
-                Budget.setBudget(self)
-                self.mainMenu()
-            elif selection==2:
-                Transaction.__init__(self)
-                self.mainMenu()
-            elif selection==3:
-                self.seeHistory()
-            elif selection==4:
-                exit
-            else:
-                print("INVALID")
-                print("Try again!")
-                self.mainMenu()
+            selection = int(input("Enter choice: "))
         except:
-            print("Write a number from 1-6 \n")
-            self.mainMenu()
+            print("Wrong format!!!")
+            mainMenu()
+
+        if selection == 1:
+            try:
+                budgetName = input("Enter budget name: ")
+                budgetAmount = int(input("Enter your budget amount: "))
+                my_budget.date = date.today()
+                my_budget.setBudget(budgetName, budgetAmount)
+                print("\n")
+            except:
+                print("Wrong format!!!")
+                mainMenu()
+
+        elif selection == 2:
+            try:
+                recordName = input("Enter transaction name: ")
+                recordAmount = int(input("Enter your transaction amount: "))
+                recordDate = input("Enter your transaction date: ")
+
+                my_record.Add_Transaction(recordName, recordAmount, recordDate)
+            except:
+                print("Wrong format!!!")
+                mainMenu()
+
+        elif selection == 3:
+            my_record.get_all_transc()
+
+        elif selection == 4:
+            my_budget.seeHistory()
+
+        elif selection == 5:
+            break
+
+        else:
+            break
 
 
-
-if name == 'main':
-
-    Ultimatetracker()
+print("\n** WELCOME TO ULTIMATE TRACKER **")
+print("WHAT DO YOU WANT TO DAY?:")
+mainMenu()
